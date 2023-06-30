69a04f58d58a043f8a83281da3fff129d49c9cbb
Hinotori91 <petra.camondo@chello.at>
2023-06-30 11:54:18 +0200

    started exam-crud-backand

---
+++ src/test/java/com/example/examcrud/ExamCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
